13337cd2f62e31e48ea6fcd9fb4ec44b8870c26e
Tripurari Pandit <tripurarikumar766@gmail.com.com>
2024-10-06 22:26:25 +0530

    Carousel adjusted

--- frontend/src/Pages/Shop.js
+++ frontend/src/Pages/Shop.js
@@ -11,7 +11,7 @@ const Shop = ()=>{
         <>
             <MyCarousel />
             <Hero />
-            <Popular />
+            <Popular /> 
             <Offers />
             <NewCollections />
             <NewsLetter />
